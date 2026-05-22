a0b6993a031706f7f49823c1f5487afdad8e0336
Nguyễn Bảo Tâm <23130286@st.hcmuaf.edu.vn>
2026-05-22 08:22:37 +0700

    Update comment ChessGame.java

--- DoAn/src/covua/ChessGame.java
+++ DoAn/src/covua/ChessGame.java
@@ -83,7 +83,9 @@ public class ChessGame {
 	
 	
 	
-
+	//9.1.4. Hệ thống kiểm tra tính hợp lệ của nước đi, 
+	//nếu nước đi hợp lệ thì hệ thống gọi hàm makeMove(Position, Position) 
+	//nội bộ để thực thi và tiếp tục bước tiếp theo.
 	// method chọn quân đi í
 	public boolean makeMove(Position start, Position end) {
 		Piece movingPiece = board.getPiece(start.getRow(), start.getColumn());
@@ -105,7 +107,8 @@ public class ChessGame {
 		//Dánh cho online
 		lastMoveSource = start;
 	    lastMoveTarget = end;
-	    /////
+  		/////9.1.5. ChessGame tạo một chuỗi mô tả nước đi (bao gồm màu quân, loại quân và tọa độ từ đâu đến đâu) 
+	    /// rồi gọi historyMoves.add(moveNotation) để lưu lại trong danh sách lịch sử.
 	    
 		String name = movingPiece.getClass().getSimpleName();
 	    String moveNotation = String.format(
